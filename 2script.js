f6ed62615d8f938d8b7aad8ba55b83d97654a250
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-22 22:52:14 +0530

    js

--- 2script.js
+++ 2script.js
@@ -1,3 +1,4 @@
+// Variables in Javascript - var, let, const
 console.log("hello and welcome back");
 
 // var a = 5; // var sirf globally scope hota hai
@@ -7,14 +8,37 @@ var b = 7;
 var c = "javascript";
 // var 55b = "github"; // invalid
 
-console.log(a);
+// console.log(a);
 // console.log(typeof a, typeof b, typeof c);
 {
     // var a = 55;
     let a = 90;
-    console.log(a);
+    // console.log(a);
 }
 
-console.log(a);
+// console.log(a);
 const a1 = 6;
 // a1 += 1; // invalid
+
+// Primitive Data types
+let x = "day";
+let y = 33;
+let z = 3.22;
+let p = true;
+let v =  undefined;
+let r = null;
+
+console.log(x, y, z, p, v, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof v, typeof r);
+
+let o = {
+    name: "dev",
+    "job role": "teacher",
+    talented: true
+}
+
+console.log(o);
+o.salary = "100rs"; // you can add key and values to the object like this!
+console.log(o);
+o.salary = "200rs";
+console.log(o);
