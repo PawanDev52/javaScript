d31911ae2ea8ca40076af365fa63c7b84104cc76
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-22 22:37:28 +0530

    js

--- 2script.js
+++ 2script.js
@@ -1,1 +1,20 @@
 console.log("hello and welcome back");
+
+// var a = 5; // var sirf globally scope hota hai
+let a = 33; // let bahar global or block me block scope hota hai
+a = a + 3;
+var b = 7;
+var c = "javascript";
+// var 55b = "github"; // invalid
+
+console.log(a);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // var a = 55;
+    let a = 90;
+    console.log(a);
+}
+
+console.log(a);
+const a1 = 6;
+// a1 += 1; // invalid
