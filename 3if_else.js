ce1ec1e2d4cc3ee9c1739219727b584e8758055a
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-25 22:22:10 +0530

    js

--- 3if_else.js
+++ 3if_else.js
@@ -7,14 +7,14 @@ console.log(a === b);
 
 let age = 10;
 let grace = 5;
-/*
+
 console.log(age + grace);
 console.log(age - grace);
 console.log(age * grace);
 console.log(age / grace);
 console.log(age ** grace);
 console.log(age % grace);
-*/
+
 
 if((age + grace) > 18){
     console.log("you can drive");
