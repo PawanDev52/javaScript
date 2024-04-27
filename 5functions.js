9ddcedb5b6151637771cd15680f5a4545f033eb7
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-27 15:44:44 +0530

    js

---
+++ 5functions.js
@@ -0,0 +1,9 @@
+console.log("learning functions!");
+
+function great(name){
+    console.log("Hello " + name + " you are great");
+    console.log("Hello " + name + " you are a good developer!");
+}
+great("hari");
+great("ravi");
+
