c525e656a5e2c9bcb39edabcaeef0d33dd38d5e0
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-27 15:53:03 +0530

    js

--- 5functions.js
+++ 5functions.js
@@ -7,3 +7,15 @@ function great(name){
 great("hari");
 great("ravi");
 
+function sum(a, b, c = 4){ // kisi parameter ko default value bhi de sakte hai
+    // console.log(a + b);
+    return a + b + c; // jab return karte hai tab dusre variable me output jata hai
+}
+
+// sum(4, 7);
+result1 = sum(5, 8);
+result2 = sum(15, 9);
+result3 = sum(2, 18, 3);
+console.log("the sum of numbers is : ", result1);
+console.log("the sum of numbers is : ", result2);
+console.log("the sum of numbers is : ", result3);
