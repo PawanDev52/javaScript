e36c71ad139cd3d29689a7fef82423b2c3fbc8c2
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-27 15:57:20 +0530

    js

--- 5functions.js
+++ 5functions.js
@@ -9,13 +9,20 @@ great("ravi");
 
 function sum(a, b, c = 4){ // kisi parameter ko default value bhi de sakte hai
     // console.log(a + b);
+    console.log(a, b, c);
     return a + b + c; // jab return karte hai tab dusre variable me output jata hai
 }
 
 // sum(4, 7);
-result1 = sum(5, 8);
+result1 = sum(5);
 result2 = sum(15, 9);
 result3 = sum(2, 18, 3);
+
 console.log("the sum of numbers is : ", result1);
 console.log("the sum of numbers is : ", result2);
 console.log("the sum of numbers is : ", result3);
+
+// aero function
+const ero = ()=>{
+    
+}
