68869b98da7f4cebf9cfc4c70ec513cd67fe102c
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-27 15:35:28 +0530

    js

--- 4loops.js
+++ 4loops.js
@@ -1,26 +1,32 @@
 console.log("learning looops");
 
-let a = 1;
-for (let i = 0; i < 20; i++) {
-    console.log(a + i);
-}
+// let a = 1;
+// for (let i = 0; i < 20; i++) {
+//     console.log(a + i);
+// }
 
-let obj = {
-    name: "developer",
-    role: "programmer",
-    company: "coders"
-};
-for (const key in obj) {
-    const element = obj[key];
-    console.log(key + " : " + element);
-}
+// let obj = {
+//     name: "developer",
+//     role: "programmer",
+//     company: "coders"
+// };
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key + " : " + element);
+// }
 
-for (const c of "developer") {
-    console.log(c);
-}
+// for (const c of "developer") {
+//     console.log(c);
+// }
 
-let x = 5;
-while (x < 10) {
-    console.log(x);
-    x++;
-}
+// let w = 5;
+// while (w < 10) {
+//     console.log(w);
+//     w++;
+// }
+
+let d = 1;
+do {
+    console.log(d);
+    d++;
+} while (d < 6);
