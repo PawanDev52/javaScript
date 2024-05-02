4446f800b44f61f3d3c4b9b5d561788bfb600254
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-02 22:22:31 +0530

    js

--- 6strings.js
+++ 6strings.js
@@ -1,8 +1,9 @@
 console.log("learning strings in js");
 
 let a = "developer";
-
 console.log(a);
 console.log(a[0]);
 console.log(a[1]);
 console.log(a[2]);
+console.log(a[8]);
+// console.log(a[9]);
