76fe9b2370411833ff08ce9c71e8890f84f71378
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-08 22:49:22 +0530

    js

--- exercise/ex10.js
+++ exercise/ex10.js
@@ -3,7 +3,14 @@
 let obj1 = ["one", "two", "three"];
 let obj2 = ["four", "five", "six"];
 let obj3 = ["seven", "eight", "nine"];
-console.log(obj1);
-console.log(obj2);
-console.log(obj3);
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj3);
 
+// for (let i = 0; i < obj1.length; i++) {
+//     // const element = obj1[i];
+//     const element = Math.random() * obj1[i];
+//     console.log(element);
+// }
+let element = Math.random() * obj1;
+console.log(element);
