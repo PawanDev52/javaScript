69191ebd3751d8fe1e9e5a8ac657e9c4025eb98a
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-08 22:44:30 +0530

    js

--- exercise/ex10.js
+++ exercise/ex10.js
@@ -1,1 +1,9 @@
 // exercise 10
+
+let obj1 = ["one", "two", "three"];
+let obj2 = ["four", "five", "six"];
+let obj3 = ["seven", "eight", "nine"];
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+
