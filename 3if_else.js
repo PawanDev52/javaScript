1303bdff619789c5555cbb339c58da443f9c2314
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-09 22:42:50 +0530

    js

--- 3if_else.js
+++ 3if_else.js
@@ -16,7 +16,6 @@ console.log(age / grace);
 console.log(age ** grace);
 console.log(age % grace);
 
-
 if((age + grace) > 18){
     console.log("you can drive");
 }
@@ -28,5 +27,4 @@ else{
 }
 
 let c = a < b ? 'yes': 'not';
-
 console.log(c);
