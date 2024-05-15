f113f73e1750c25738789b931537ee13536d0436
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-15 22:37:57 +0530

    js

--- exercise/ex10.js
+++ exercise/ex10.js
@@ -59,9 +59,15 @@ let third = {
     3 : "nine"
 }
 let printRandom = Math.floor(Math.random() * 3 + 1);
+console.log(printRandom);
 let var1 = first[printRandom];
+
 printRandom = Math.floor(Math.random() * 3 + 1);
+console.log(printRandom);
 let var2 = second[printRandom];
+
 printRandom = Math.floor(Math.random() * 3 + 1);
+console.log(printRandom);
 let var3 = third[printRandom];
+
 console.log(`random word is : ${var1} ${var2} ${var3}`);
