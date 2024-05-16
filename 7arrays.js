6ce069ec781af35d140df274df04f53a72eb0f07
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-16 22:23:09 +0530

    js

--- 7arrays.js
+++ 7arrays.js
@@ -5,10 +5,5 @@ console.log("Arrays in JS");
 let arr = [1,2,3,4,5,7];
 
 console.log(arr);
-// console.log(arr.length);
+console.log(arr.length);
 
-// console.log(018 == '018');
-
-// console.log(017 == '017');
-
-// console.log(017 == '015');
