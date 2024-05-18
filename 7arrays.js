bf24ae9bd44ce892a7344ff9a7e794071fcb3869
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-18 17:05:28 +0530

    js

--- 7arrays.js
+++ 7arrays.js
@@ -1,19 +1,9 @@
 // Arrays method in JS
 
 let arr = [1,2,3,4,5,7];
-
 arr[0] = 5666;
 
 console.log(arr);
-
 console.log(arr.length);
-
 console.log(arr[0]);
-
 console.log(arr[1]);
-
-console.log(arr[2]);
-
-// Array methods
-
-console.log("some array methods!");
