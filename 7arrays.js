ac97d66d2fe093cbdc29483631d6588d93cd1a72
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-18 17:14:08 +0530

    js

--- 7arrays.js
+++ 7arrays.js
@@ -1,12 +1,13 @@
 // Arrays method in JS
 
 let arr = [1,2,3,4,5,7];
-arr[0] = 5666;
+// arr[0] = 5666;
 
-console.log(arr);
-console.log(arr.length);
-console.log(arr[0]);
-console.log(arr[1]);
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
 
 // Array methods
 console.log(arr.toString());
+console.log(arr.join(" - ")); // it will addd - after every array values
