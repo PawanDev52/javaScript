5abb90b9193ea5598891145e07799f0f5e977841
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-18 17:27:49 +0530

    js

--- 7arrays.js
+++ 7arrays.js
@@ -1,6 +1,8 @@
 // Arrays method in JS
 
 let arr = [1,2,3,4,5,7];
+let arr2 = [4,7,8,9];
+let arr3 = [0,9,4,5];
 // arr[0] = 5666;
 
 // console.log(arr);
@@ -9,8 +11,11 @@ let arr = [1,2,3,4,5,7];
 // console.log(arr[1]);
 
 // Array methods
-console.log(arr.toString());
-console.log(arr.join(" - ")); // it will add - after every array elements
-console.log(arr.pop()); // removes last element from the array
-console.log(arr.push('kid')); // it will add new element in the end but return the new length
+// console.log(arr.toString());
+// console.log(arr.join(" - ")); // it will add - after every array elements
+// console.log(arr.pop()); // removes last element from the array
+// console.log(arr.push('kid')); // it will add new element in the end but return the new length
+// console.log(arr.shift()); // removes first element and return it
+// console.log(arr.unshift(true)); // add element in starting and return length
+// delete arr[2]; // delete an element / memory is allocated in array but there is no value
 console.log(arr);
