68c14171b627dab789f20b76c41713ef49f76fa9
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-21 22:15:27 +0530

    js

--- 7arrays.js
+++ 7arrays.js
@@ -5,20 +5,22 @@ let arr2 = [4,7,8,9];
 let arr3 = [0,9,4,5];
 // arr[0] = 5666;
 
-console.log(arr);
-console.log(arr.length);
-console.log(arr[0]);
-console.log(arr[1]);
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
 
 // Array methods
 // console.log(arr.toString());
 // console.log(arr.join(" - ")); // it will add - after every array elements
-console.log(arr.pop()); // removes last element from the array
-console.log(arr.push('kid')); // it will add new element in the end but return the new length
-console.log(arr.shift()); // removes first element and return it
+// console.log(arr.pop()); // removes last element from the array
+// console.log(arr.push('kid')); // it will add new element in the end but return the new length
+// console.log(arr.shift()); // removes first element and return it
 // console.log(arr.unshift(true)); // add element in starting and return length
-delete arr[2]; // delete an element / memory is allocated in array but there is no value
-console.log(arr.concat(arr2,arr3)); // doesn't modify original array
+// delete arr[2]; // delete an element / memory is allocated in array but there is no value
+// console.log(arr.concat(arr2,arr3)); // doesn't modify original array
 console.log(arr);
-console.log(arr.join(" and "));
+// console.log(arr.join(" and "));
 console.log(arr.sort());
+console.log(arr.reverse());
+console.log(arr.toSorted());
