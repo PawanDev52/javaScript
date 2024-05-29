02cfa3aae31e6587cfe5b049d7972dec108de5ac
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-05-29 22:50:23 +0530

    js

--- exercise/ex11.js
+++ exercise/ex11.js
@@ -1,7 +1,14 @@
 // exercise - 11
 
 // using reduce method
-let a = [1,2,3,4,5,6,7,8,9];
-let b = a.reduce((total, num) => total*num);
-console.log(b);
+// let a = [1,2,3,4,5,6,7,8,9];
+// let b = a.reduce((total, num) => total*num);
+// console.log(b);
 
+// using for loop method
+let a = [1,2,3,4,5,6,7,8,9];
+let b = 1;
+for (let i = 1; i <= a.length; i++) {
+    b = b * i;
+}
+console.log(b);
