fe31b5d9ee2e340eddcda4ae0b356daaac76a729
Pawan Sharma <133631214+PawanDev52@users.noreply.github.com>
2024-07-03 20:47:39 -0700

    js

--- 16promise.js
+++ 16promise.js
@@ -1,12 +1,33 @@
 // promise
 
-let prom = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
-    console.log('yes its done')
-    resolve("code")
-  }, 3000);
+// let prom = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     console.log('yes its done')
+//     resolve("code")
+//   }, 3000);
+// });
+
+// prom.then((a)=>{
+//   console.log(a);
+// })
+
+// ==================================
+// try with catch()
+
+let prom = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("no random number supporting");
+  } else {
+    setTimeout(() => {
+      console.log("yes its done");
+      resolve("code");
+    }, 3000);
+  }
 });
 
-prom.then((a)=>{
+prom.then((a) => {
   console.log(a);
+}).catch((err)=>{
+    console.log(err)
 })
