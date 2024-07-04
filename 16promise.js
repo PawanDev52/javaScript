1d4c52859cb1489e71d03f9e223760adc73ace56
Pawan Sharma <133631214+PawanDev52@users.noreply.github.com>
2024-07-03 20:40:46 -0700

    js

--- 16promise.js
+++ 16promise.js
@@ -1,1 +1,12 @@
 // promise
+
+let prom = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    console.log('yes its done')
+    resolve("code")
+  }, 3000);
+});
+
+prom.then((a)=>{
+  console.log(a);
+})
